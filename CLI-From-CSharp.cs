36d3a337a720e523741c52592a834fcfc72c85c8
Joey <jyw22@ic.ac.uk>
2025-07-09 11:01:20 +0100

    Working C# to CLI echo; next: work on adapting this for arduino CLI

---
+++ CLI-From-CSharp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        var psi = new ProcessStartInfo
+        {
+            FileName = "cmd.exe",
+            Arguments = "/C echo konichiwaa",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = false
+        };
+
+        using (Process proc = new Process { StartInfo = psi })
+        {
+            proc.Start();
+            string output = proc.StandardOutput.ReadToEnd();
+            string error = proc.StandardError.ReadToEnd();
+            proc.WaitForExit();
+
+            Console.WriteLine("Output: \n" + output);
+            if (!string.IsNullOrEmpty(error))
+            {
+                Console.WriteLine("Error: \n" + error);
+            }
+        }
+    }
+
+}
+
+
